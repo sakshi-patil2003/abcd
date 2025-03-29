fbef1c52c3950d0aff6e4ca95dff5ac63a8d1df2
Deep-mahire <Dipratnamahire2222@gmail.com>
2025-03-29 17:11:16 +0530

    Push

--- src/components/Computer/CompRecruiter.jsx
+++ src/components/Computer/CompRecruiter.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import logo1 from "../../assets/images/IT-Com-AIDS/palo (1).avif";
-import logo2 from "../../assets/Images/IT-Com-AIDS/yash.AVIF";
+import logo2 from "../../assets/Images/IT-Com-AIDS/yash.avif";
 import logo3 from "../../assets/Images/IT-Com-AIDS/philips.AVIF";
 import logo4 from "../../assets/images/IT-Com-AIDS/jaspay.avif";
 import logo5 from "../../assets/Images/IT-Com-AIDS/zs.avif";
