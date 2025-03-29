2db1237d02c1a0476a1779c7e1bdb64609fa4c84
Deep-mahire <Dipratnamahire2222@gmail.com>
2025-03-29 17:35:17 +0530

    Push

--- src/components/Computer/CompRecruiter.jsx
+++ src/components/Computer/CompRecruiter.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import logo1 from "../../assets/images/IT-Com-AIDS/palo.AVIF";
+// import logo1 from "../../assets/images/IT-Com-AIDS/palo.AVIF";
 import logo2 from "../../assets/Images/IT-Com-AIDS/yash.AVIF";
 import logo3 from "../../assets/Images/IT-Com-AIDS/philips.AVIF";
 import logo4 from "../../assets/images/IT-Com-AIDS/jaspay.AVIF";
@@ -32,7 +32,7 @@ const sponsorsData = [
   {
     title: "IT RECRUITERS",
     logos: [
-      logo1,
+      // logo1,z
       logo2,
       logo3,
       logo4,
